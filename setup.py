950fed179ecd27bb43b103ed90545291225dbc45
Shrinivas <kulkarnishrinivas66@gmail.com>
2023-11-21 09:09:53 +0530

    setup

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setuptools.setup(
     name='audio-speech-to-sign-language-converter',
     version='0.1.0',
     description='Python project',
-    author='Jigar Gajjar',
-    author_email='jigargajjar.55jg@gmail.com',
-    url='https://github.com/jigargajjar55/Audio-Speech-To-Sign-Language-Converter',
+    author='Shrinivas Kulkarni',
+    author_email='kulkarnishrinivas66@gmail.com',
+    url='https://github.com/Shrini66/BE_Project',
     packages=setuptools.find_packages(),
     setup_requires=['nltk', 'joblib','click','regex','sqlparse','setuptools'],
 )
